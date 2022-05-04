b44cc251b0c8281fd75974d9b3475dbcfb8b680f
FilipeGomesMelo <fgm3@cin.ufpe.br>
2022-05-04 15:14:08 -0300

    Atualização do servidor

--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -1,59 +1,15 @@
 import express = require('express');
 
-import { Aluno } from '../common/aluno';
-import { CadastroDeAlunos } from './cadastrodealunos';
-import { CadastroDeProfessor } from './cadastrodeprofs';
-
 var taserver = express();
 
-var cadastroaluno: CadastroDeAlunos = new CadastroDeAlunos();
-var cadastroprof: CadastroDeProfessor = new CadastroDeProfessor();
+var alunos = [{nome:'Paulo',cpf:'683',email:'phmb@cin.br',metas:{'requisitos':'MA'}},{nome:'Mariana',cpf:'456',email:'@mcb@cin.br',metas:{'requisitos':'MPA'}}];
 
-var allowCrossDomain = function(req: any, res: any, next: any) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-taserver.use(allowCrossDomain);
-
-taserver.use(express.json());
-
-
-taserver.get('/alunos', function (req: express.Request, res: express.Response) {
-  res.send(JSON.stringify(cadastroaluno.getAlunos()));
+taserver.get('/', function (req, res) {
+  res.send(alunos);
 })
 
-taserver.get('/professores', function (req: express.Request, res: express.Response) {
-  res.send(JSON.stringify(cadastroprof.getProfessores()));
+taserver.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 })
 
-taserver.post('/aluno', function (req: express.Request, res: express.Response) {
-  var aluno: Aluno = <Aluno> req.body; //verificar se é mesmo Aluno!
-  aluno = cadastroaluno.cadastrar(aluno);
-  if (aluno) {
-    res.send({"success": "O aluno foi cadastrado com sucesso"});
-  } else {
-    res.send({"failure": "O aluno não pode ser cadastrado"});
-  }
-})
 
-taserver.put('/aluno', function (req: express.Request, res: express.Response) {
-  var aluno: Aluno = <Aluno> req.body;
-  aluno = cadastro.atualizar(aluno);
-  if (aluno) {
-    res.send({"success": "O aluno foi atualizado com sucesso"});
-  } else {
-    res.send({"failure": "O aluno não pode ser atualizado"});
-  }
-})
-
-var server = taserver.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
-function closeServer(): void {
-  server.close();
-}
-
-export { server, closeServer }
